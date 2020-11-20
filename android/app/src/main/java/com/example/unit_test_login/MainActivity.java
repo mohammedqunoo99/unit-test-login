9b0f1a862d2b52b05070448f541350dcde2f15ae
Mohammed <Mohammed.Qunoo99@gmail.com>
2020-11-20 15:49:43 -0800

    unit-test

---
+++ android/app/src/main/java/com/example/unit_test_login/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.unit_test_login;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_auth/ui/pages/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'model/user_repository.dart';
+import 'ui/pages/login.dart';
+
+void main() => runApp(MyApp(
+
+));
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      builder: (_) => UserRepository.instance(auth: FirebaseAuth.instance),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage(),
+      ),
+    );
+  }
+}
